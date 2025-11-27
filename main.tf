d585f7569d7791153a4bf369e2c4774311e8e730
kaveesha <kalshandhanasekara@gmail.com>
2025-11-27 16:25:06 +0530

    add vpc and subnet

--- main.tf
+++ main.tf
@@ -1,40 +1,24 @@
-provider "aws" {}
-
-variable "data" {
-  description = "all cidr blocks and names"
-    # default = "10.0.0.1/16"
-    type = list(object({
-      cidr_block = string
-      name = string
-    }))
+provider "aws" {
+  region = "ap-southeast-1"
 }
 
-variable az {
-  default = "ap-southeast-1a"
-}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable az {}
+variable "env_prefix" {}
 
-resource "aws_vpc" "test_vpc" {
-  cidr_block       = var.data[0].cidr_block
-  instance_tenancy = "default"
-
+resource "aws_vpc" "my_vpc" {
+  cidr_block       = var.vpc_cidr_block
   tags = {
-    Name = var.data[0].name
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "test_subnet_1" {
-  vpc_id            = aws_vpc.test_vpc.id
-  cidr_block        = var.data[1].cidr_block
+resource "aws_subnet" "my_subnet_1" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.az
   tags = {
-    Name = var.data[1].name
+    Name = "${var.env_prefix}-subnet-1"
   }
-}
-
-output "test_vpc_id" {
-  value = aws_vpc.test_vpc.id
-}
-
-output "aws_subnet_id" {
-  value = aws_subnet.test_subnet_1.id
 }
