81d307eb23d4e7d80e95bdf31d5c9d9fe3eae16a
kaveesha <kalshandhanasekara@gmail.com>
2025-11-28 04:38:40 +0530

    added igw to default-rtb

--- main.tf
+++ main.tf
@@ -23,17 +23,6 @@ resource "aws_subnet" "my_subnet_1" {
   }
 }
 
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_internet_gateway.id
-  }
-  tags = {
-    Name = "${var.env_prefix}-rtb"
-  }
-}
-
 resource "aws_internet_gateway" "my_internet_gateway" {
   vpc_id = aws_vpc.my_vpc.id
   tags = {
@@ -41,7 +30,13 @@ resource "aws_internet_gateway" "my_internet_gateway" {
   }
 }
 
-resource "aws_route_table_association" "a_rtb_subnet_1" {
-  subnet_id      = aws_subnet.my_subnet_1.id
-  route_table_id = aws_route_table.my_route_table.id  
+resource "aws_default_route_table" "main_rtb" {
+  default_route_table_id = aws_vpc.my_vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_internet_gateway.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
 }
