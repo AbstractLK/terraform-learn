2d4a2827c1384def281c6d86c5f07475c615dc9a
kaveesha <kalshandhanasekara@gmail.com>
2025-11-28 05:32:14 +0530

    added rules to new sg

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable az {}
 variable "env_prefix" {}
+variable "my_ip" {}
 
 resource "aws_vpc" "my_vpc" {
   cidr_block       = var.vpc_cidr_block
@@ -38,5 +39,34 @@ resource "aws_default_route_table" "main_rtb" {
   }
   tags = {
     Name = "${var.env_prefix}-main-rtb"
+  }
+}
+
+resource "aws_security_group" "my_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+  name = "my-sg"
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ var.my_ip ]
+  }
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+    prefix_list_ids = []
+  }
+  tags = {
+    Name = "${var.env_prefix}-sg"
   }
 }
