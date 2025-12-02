d362e27f1a955ea951c900013fb2b587a227eec1
kaveesha <kalshandhanasekara@gmail.com>
2025-12-02 08:55:29 +0530

    remote-exec, local-exec and file provisioner

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
 variable "public_key_path" {}
+variable "private_key_path" {}
 
 resource "aws_vpc" "my_vpc" {
   cidr_block       = var.vpc_cidr_block
@@ -109,8 +110,6 @@ resource "aws_instance" "my-server" {
     Name = "${var.env_prefix}-server"
   }
 
-  user_data = file("entry-script.sh")
-
   /* user_data = <<-EOF
                 #!/bin/bash
                 sudo yum update -y
@@ -121,7 +120,33 @@ resource "aws_instance" "my-server" {
                 docker run -d -p 8080:80 --name webserver nginx
               EOF */
 
+  # user_data = file("entry-script.sh")
+
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "ec2-user"
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "file" {
+    source = "entry-script.sh"
+    destination = "/home/ec2-user/script.sh"
+  }
+
+  provisioner "remote-exec" {
+      inline = [ 
+        "chmod +x /home/ec2-user/script.sh",
+        "/home/ec2-user/script.sh"
+      ]
+  }
+
+  provisioner "local-exec" {
+    command = "echo EC2 Instance Public IP: ${self.public_ip} > ec2_ip.txt"
+  }
+
   /* lifecycle {
     ignore_changes = [ami]
   } */
+
 }
